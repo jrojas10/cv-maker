4a224a172c33d6511a9186be41890bdd6893ac84
juan rojas <jrojasbf@gmail.com>
2021-08-28 22:04:46 -0700

    updated file names

---
+++ src/components/Personal_info.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+class Personal_info extends Component {
+    constructor(props)
+    super(props)
+    render() {
+
+        const { }
+        return (
+
+            <div>
+                <form>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default Personal_info
