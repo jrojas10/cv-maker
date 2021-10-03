da3ff84d2d04663d499c9f05a4c1855d1193a31d
juan rojas <jrojasbf@gmail.com>
2021-10-02 23:40:19 -0700

    passed props for personal info form

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,14 @@
 
-function Display() {
+function Display(props) {
+
     return (
         <div>
             <h1>My Resume</h1>
+            <h3>First Name :{props.fname}</h3>
+            <h3>info : {props.info}</h3>
         </div>
     )
 }
+//use this.props when using the class extends
 
 export default Display;
--- src/components/Personalinfo.js
+++ src/components/Personalinfo.js
@@ -1,4 +1,5 @@
-import React, { Component, useRef } from "react";
+import React, { } from "react";
+import Display from "./Display";
 
 class Personalinfo extends React.Component {
     //function Personalinfo() {
@@ -7,7 +8,7 @@ class Personalinfo extends React.Component {
         this.state = {
             firstname: '',
             lastname: '',
-            phoneNumber: '',
+            phonenumber: '',
             email: '',
             address: '',
             state: ''
@@ -16,11 +17,22 @@ class Personalinfo extends React.Component {
     handleChange = ({ target }) => {
         this.setState({ [target.name]: target.value });
     }
+    // import {useRef} from "react"
     //const firstNameInputRef = useRef();
     // const enteredFirstName = firstNameInputRef.current.value; add ref = {firstNameInputRef} attribute to input tag
 
 
     render() {
+        const info = {
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            phonenumber: this.state.phonenumber,
+            email: this.state.email,
+            address: this.state.address,
+            state: this.state.state
+        }
+
+        const input = [info.firstname, info.lastname, info.phonenumber, info.email, info.address, info.state];
         return (
             <div>
                 <h2>Personal Information</h2>
@@ -33,7 +45,7 @@ class Personalinfo extends React.Component {
                         <input className="form-control" name="lastname" type="text" value={this.state.lastname} onChange={this.handleChange}></input>
 
                         <label className="form-label">Phone Number</label>
-                        <input className="form-control" name="phonenumber" type="number" value={this.state.phoneNumber} onChange={this.handleChange}></input>
+                        <input className="form-control" name="phonenumber" type="tel" value={this.state.phoneNumber} onChange={this.handleChange}></input>
 
                         <label className="form-label">Email</label>
                         <input className="form-control" name="email" type="email" value={this.state.email} onChange={this.handleChange}></input>
@@ -46,12 +58,18 @@ class Personalinfo extends React.Component {
                     </div>
 
                 </form>
-                <h3>First Name: {this.state.firstname}</h3>
+
+                {/* 'firstname' : {this.state.firstname},
+                'lastname' : {this.state.lastname} */}
+
+
+                <Display fname={this.state.firstname} info={input} />
+                {/* <h3>First Name: {this.state.firstname}</h3>
                 <h3>Last Name : {this.state.lastname}</h3>
                 <h3>Phone Number: {this.state.phoneNumber}</h3>
                 <h3>Email: {this.state.email}</h3>
                 <h3>Address: {this.state.address}</h3>
-                <h3>State: {this.state.state}</h3>
+                <h3>State: {this.state.state}</h3> */}
 
             </div>
         );
